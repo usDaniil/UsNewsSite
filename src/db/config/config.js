3a100174fd327c4c1f15b059f98ca25da73b9538
dunice1129 <dunice1129@Dunices-Mac-mini.local>
2023-08-07 17:53:15 +0300

    day commit 07.08.23

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -1,8 +1,8 @@
-module.export = {
+module.exports = {
+  dialect: 'postgres',
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
-  dialect: process.env.DB_DIALECT,
 };
